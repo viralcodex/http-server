e026da735a87cf0581f03b0051a640225191fa01
Aviral Shukla <aviral.shukla2122000@gmail.com>
2026-01-08 00:46:25 +0530

    Step 2: Newlines
    Refactored the file reading logic to accumulate and print lines split across 8-byte chunks, ensuring lines are printed correctly even if they span multiple reads. Removed premature System.exit call and added final flush for any remaining data after EOF.

--- src/main/java/com/bootdev/httpserver/HttpServer.java
+++ src/main/java/com/bootdev/httpserver/HttpServer.java
@@ -7,23 +7,34 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 class HttpServer {
-    public void run(){
-        try(InputStream inputStream = new FileInputStream("messages.txt"))
-        {
+    public void run() {
+        try (InputStream inputStream = new FileInputStream("messages.txt")) {
             //have to read in 8 bytes chunks from the file until EOF
             byte[] buffer = new byte[8]; //buffer to hold 8 bytes
-            while(true){
+            StringBuilder currentLine = new StringBuilder();
+            while (true) {
                 int bytesRead = inputStream.read(buffer);
-                if( bytesRead == -1) //end of file
+                if (bytesRead == -1) //end of file
                 {
-                    System.out.println("End of File, exiting...");
-                    System.exit(0);
+                    break;
                 }
                 String chunk = new String(buffer, 0, bytesRead, StandardCharsets.UTF_8);
-                System.out.printf("read: %s\n", chunk);
+                String[] parts = chunk.split("\n"); //split on end of line
+
+                //add these parts(not the last one as it will be of next line)
+                for (int i = 0; i < parts.length - 1; i++) {
+                    currentLine.append(parts[i]);
+                    System.out.printf("read: %s\n", currentLine);
+                    currentLine.setLength(0); //reset currentLine
+                }
+                currentLine.append(parts[parts.length - 1]);
             }
-        } catch (FileNotFoundException e)
-        {
+            if (!currentLine.isEmpty()) //after EOF if data is there in it, flush it
+            {
+                System.out.printf("read: %s\n", currentLine);
+            }
+
+        } catch (FileNotFoundException e) {
             System.out.println("File not found: " + e.getMessage());
         } catch (IOException e) {
             throw new RuntimeException(e);
