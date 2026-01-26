51e8bc7a0f352b9e5cb51f3513e6d79edcf3c11e
Aviral Shukla <aviral.shukla2122000@gmail.com>
2026-01-26 22:06:21 +0530

    Reapply "Add HTTP request body parsing and related tests"
    This reverts commit 81fa64349d84fe232939def471186b9607443d96.

--- src/main/java/com/bootdev/httpserver/HTTPServer.java
+++ src/main/java/com/bootdev/httpserver/HTTPServer.java
@@ -26,11 +26,10 @@ public class HttpServer {
 
     public void handleConnection(Socket socket) {
         try (socket) {
-            InputStreamReader inputStreamReader = new InputStreamReader(
-                    socket.getInputStream(), StandardCharsets.UTF_8
-            );
+            InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
             RequestFromReader requestFromReader = new RequestFromReader();
             Request request = requestFromReader.requestFromReader(inputStreamReader);
+            String body = new String(request.getBody(), StandardCharsets.UTF_8);
 
             System.out.println("Request Line:");
             System.out.println("- Method: " + request.getRequestLine().getMethod());
@@ -40,7 +39,8 @@ public class HttpServer {
             for (Map.Entry<String, String> entry : request.getHeaders().entrySet()) {
                 System.out.printf("- %s: %s\n", entry.getKey(), entry.getValue());
             }
-            System.out.println("Connection closed");
+            System.out.println("Body: ");
+            System.out.println(body);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- src/main/java/com/bootdev/internal/request/ParserState.java
+++ src/main/java/com/bootdev/internal/request/ParserState.java
@@ -3,5 +3,6 @@ package com.bootdev.internal.request;
 public enum ParserState {
     INITIALIZED,
     PARSING_HEADERS,
+    PARSING_BODY,
     DONE
 }
